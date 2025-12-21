6b94328c3f92e5dc62b8b06e09e778edb75da366
Mengzi Li <limengzi96@gmail.com>
2025-12-22 00:28:23 +0800

    basic shape

---
+++ src/mzcst_2024/utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+This module provides basic geometric shapes for use in the mzcst_2024 package.
+"""
+
+from ._basic_shapes import Point, Line2D
+
--- src/mzcst_2024/utils/_basic_shapes.py
+++ src/mzcst_2024/utils/_basic_shapes.py
@@ -1,5 +1,4 @@
 """
-mzcst_2024.utils._basic_shapes 的 Docstring
 
 This module provides basic geometric shapes for use in the mzcst_2024 package.
 """
