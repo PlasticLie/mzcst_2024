bc427fa31fbe57e9a16ebf1b99472dc92aa77532
Mengzi Li <limengzi96@gmail.com>
2026-01-15 18:01:01 +0800

    Update check_interface.py

--- tests/check_interface.py
+++ tests/check_interface.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
     LOG_LEVEL = logging.INFO
-    FMT = "%(asctime)s.%(msecs)-3d %(name)s: %(levelname)s: %(message)s"
+    FMT = "%(asctime)s.%(msecs)-3d %(name)s %(lineno)4d: %(levelname)s: %(message)s"
     DATEFMT = r"%Y-%m-%d %H:%M:%S"
     LOG_FORMATTER = logging.Formatter(FMT, DATEFMT)
     logging.basicConfig(
@@ -41,7 +41,22 @@ if __name__ == "__main__":
 
     logger.info("Created new DesignEnvironment with PID: %s", f"{new_env.pid_}")
     logger.info("CST Version: %s", new_env.version())
-    logger.info("Is in quiet mode: %s", new_env.in_quiet_mode())
+    logger.info("Is in quiet mode: %s", new_env.in_quiet_mode_)
+
+    envs.append(new_env)
+
+    # endregion
+    # ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+    #######################################
+    # region 打开已有的项目
+    # ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+    PROJECT_ABSOLUTE_PATH: str = r"D:\CST-2024-local\fss-rumpf-local"
+    filename: str = "flat-demo-20260112-175322.cst"
+    fullname: str = os.path.join(PROJECT_ABSOLUTE_PATH, filename)
+
+    envs[-1].open_project(fullname)
 
     # endregion
     # ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
@@ -53,7 +68,7 @@ if __name__ == "__main__":
     timestamps.append(time.perf_counter())
     elapsed = timestamps[-1] - timestamps[0]
     logger.info(
-        "Finished %s, total elapsd time: %s.",
+        "Finished %s, total elapsed time: %s.",
         __file__,
         mz.common.time_to_string(elapsed),
     )
