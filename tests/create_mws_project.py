06dd8b59296ff439ebbdf3e8c92e19d6d79dcedf
Mengzi Li <limengzi96@gmail.com>
2026-03-02 17:09:13 +0800

    Update create_mws_project.py

--- tests/create_mws_project.py
+++ tests/create_mws_project.py
@@ -633,7 +633,7 @@ if __name__ == "__main__":
     # ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
     bg = solver.Background(
-        attributes={"Type": '"normal"'}, Type='"normal"'
+        attributes={"Type": '"normal"'}
     ).create_from_attributes(m3d)
 
     bd = solver.Boundary(
