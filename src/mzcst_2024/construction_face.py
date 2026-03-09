5ee876b4aeab897e246dfc93210079eaf9bb058a
Mengzi Li <limengzi96@gmail.com>
2026-03-09 17:36:48 +0800

    Update construction_face.py

--- src/mzcst_2024/construction_face.py
+++ src/mzcst_2024/construction_face.py
@@ -8,6 +8,8 @@ from . import interface
 from .common import NEW_LINE
 from .global_ import BaseObject, ParameterLike
 
+_logger = logging.getLogger(__name__)
+
 
 class Face(BaseObject):
     """Defines a Face object"""
@@ -31,3 +33,24 @@ class Face(BaseObject):
         self._twist_angle = str(twist_angle)
         self._type = type_
         return
+
+    def create(self, modeler: "interface.Model3D") -> "Face":
+        """Creates the face in the modeler."""
+        scmd = [
+            "With Face",
+            ".Reset",
+            f'.Name "{self._name}"',
+            f'.Curve "{self._curve_name}"',
+            f".Offset {self._offset}",
+            f".TaperAngle {self._taper_angle}",
+            f".Thickness {self._thickness}",
+            f".TwistAngle {self._twist_angle}",
+            f'.Type "{self._type}"',
+            ".Create",
+            "End With",
+        ]
+        cmd = NEW_LINE.join(scmd)
+        self._history.append(f'create face "{self._name}"')
+        modeler.add_to_history(self._history[-1], cmd)
+        _logger.info(self._history[-1])
+        return self
