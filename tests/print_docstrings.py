2236c8fa24fdb9d5d5ba781c4db873e0d532aacb
Mengzi Li <limengzi96@gmail.com>
2026-04-01 20:34:49 +0800

    Update print_docstrings.py

--- tests/print_docstrings.py
+++ tests/print_docstrings.py
@@ -13,32 +13,32 @@ from mzcst_2024.utils.docstring_printers import (
 if __name__ == "__main__":
 
     RESULTS_DIR = "results"
-    with open(
-        f"{RESULTS_DIR}/units_docstrings.txt", "w", encoding="utf-8"
-    ) as f:
-        printer = ModuleDocstringPrinter(cu, f, False)
-        printer.print_all()
+    # with open(
+    #     f"{RESULTS_DIR}/units_docstrings.txt", "w", encoding="utf-8"
+    # ) as f:
+    #     printer = ModuleDocstringPrinter(cu, f, False)
+    #     printer.print_all()
 
-    with open(
-        f"{RESULTS_DIR}/interface_docstrings.txt", "w", encoding="utf-8"
-    ) as f:
-        printer = ModuleDocstringPrinter(ci, f, False)
-        printer.print_all()
+    # with open(
+    #     f"{RESULTS_DIR}/interface_docstrings.txt", "w", encoding="utf-8"
+    # ) as f:
+    #     printer = ModuleDocstringPrinter(ci, f, False)
+    #     printer.print_all()
 
-    with open(
-        f"{RESULTS_DIR}/model3d_docstrings.txt", "w", encoding="utf-8"
-    ) as f:
-        de = sign_env = ci.DesignEnvironment.connect_to_any_or_new()
-        prj = de.active_project()
-        m3d = prj.model3d
-        printer = ObjectDocstringPrinter(m3d, f, False)
-        # printer.print_methods()
-        printer.print_attributes_and_methods()
+    # with open(
+    #     f"{RESULTS_DIR}/model3d_docstrings.txt", "w", encoding="utf-8"
+    # ) as f:
+    #     de = sign_env = ci.DesignEnvironment.connect_to_any_or_new()
+    #     prj = de.active_project()
+    #     m3d = prj.model3d
+    #     printer = ObjectDocstringPrinter(m3d, f, False)
+    #     # printer.print_methods()
+    #     printer.print_attributes_and_methods()
 
-    with open(
-        f"{RESULTS_DIR}/results_docstrings.txt", "w", encoding="utf-8"
-    ) as f:
-        printer = ModuleDocstringPrinter(cr, f, False)
-        printer.print_classes_and_methods()
-        printer.print_functions()
+    # with open(
+    #     f"{RESULTS_DIR}/results_docstrings.txt", "w", encoding="utf-8"
+    # ) as f:
+    #     printer = ModuleDocstringPrinter(cr, f, False)
+    #     printer.print_classes_and_methods()
+    #     printer.print_functions()
     pass
